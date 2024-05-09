d15937ed449a4490a87f7252cdd59aa6308da076
Humoyun Abduqodirov <abduodirovhumoyun@gmail.com>
2024-05-09 13:17:29 +0500

    2 added schema for the data

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,46 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicid: string;
+  secureUrl: URL;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicid: { type: String, required: true },
+    secureUrl: { type: URL, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    //   createdAt: { type: Date, default: Date.now },
+    //   updatedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
